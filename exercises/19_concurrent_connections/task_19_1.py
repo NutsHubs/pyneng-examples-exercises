81c950570475856c106c9f4e686d35bc145976a1
Abysscope <abysscope@gmail.com>
2021-10-06 22:06:37 +0300

    checkout

--- exercises/19_concurrent_connections/task_19_1.py
+++ exercises/19_concurrent_connections/task_19_1.py
@@ -24,14 +24,41 @@
 IP-адресов с помощью concurrent.futures (это надо сделать в функции ping_ip_addresses).
 """
 import subprocess
+import concurrent.futures as conc
+import logging
+from datetime import datetime
+import time
 
-def ping_ip_addresses(ip_list:list, limit=3):
-    result = list(), list()
-    pass
+logging.basicConfig(
+    format='%(threadName)s %(name)s: %(message)s',
+    level=logging.INFO
+)
 
 
-def ping_check(address):
-    check = False
-    subprocess.run('dir', stdout=subprocess.PIPE)
-    return check
+def ping_ip_addresses(ip_list, thread_max=3):
+    reach = list()
+    unreach = list()
+    logging.info(f'function are starting on {datetime.now().time()}')
+    with conc.ThreadPoolExecutor(max_workers=thread_max) as executor:
+        result = executor.map(ping_ip_address, ip_list)
+        for ip, code in zip(ip_list, result):
+            if code==0:
+                reach.append(ip)
+            else:
+                unreach.append(ip)
+    logging.info(f'function was finish on {datetime.now().time()}')
+    return reach, unreach
+
+
+def ping_ip_address(address):
+    logging.info(f'"ping {address}" are starting on {datetime.now().time()}')
+    result = subprocess.run(['ping', '-c', '3', address], stdout=subprocess.PIPE)
+    logging.info(f'"ping {address}" was finish on {datetime.now().time()}')
+    return result.returncode
+
+
+if __name__ == '__main__':
+    x = ping_ip_addresses(['8.8.8.8', '8.8.8.9', '192.168.50.1', '192.168.50.3', '192.168.1.1'], 3)
+    print(x)
+
 
