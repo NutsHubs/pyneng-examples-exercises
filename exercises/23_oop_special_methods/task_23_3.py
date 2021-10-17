df944c391e54aea4980a1c7d12a1d98692c0e2d9
Abysscope <abysscope@gmail.com>
2021-10-18 00:00:45 +0300

    processing

--- exercises/23_oop_special_methods/task_23_3.py
+++ exercises/23_oop_special_methods/task_23_3.py
@@ -58,6 +58,7 @@ Out[9]:
 In [10]: t2.topology
 Out[10]: {('R1', 'Eth0/4'): ('R7', 'Eth0/0'), ('R1', 'Eth0/6'): ('R9', 'Eth0/0')}
 """
+from pprint import pprint
 
 topology_example = {
     ("R1", "Eth0/0"): ("SW1", "Eth0/1"),
@@ -75,3 +76,63 @@ topology_example2 = {
     ("R1", "Eth0/4"): ("R7", "Eth0/0"),
     ("R1", "Eth0/6"): ("R9", "Eth0/0"),
 }
+
+
+class Topology:
+    def __init__(self, topology_dict):
+        self.topology = self._normalize(topology_dict)
+
+    def add_link(self, local, remote):
+        is_exists = False
+        if local in self.topology:
+            if self.topology[local] == remote:
+                print('Такое соединение существует')
+                return
+            print('Cоединение с одним из портов существует')
+            return
+        self.topology[local] = remote
+
+
+    def delete_link(self, local, remote):
+        key = local
+        if not(local in self.topology):
+            if not(remote in self.topology):
+                print('Такого соединения нет')
+                return
+            else:
+                key = remote
+        self.topology.pop(key)
+
+    def delete_node(self, node):
+        is_exists = False
+        for key in list(self.topology):
+            node_delete = key
+            if not(node in list(key)):
+                if not(node in list(self.topology[key])):
+                    continue
+            is_exists = True
+            self.topology.pop(node_delete)
+        if not is_exists:
+            print('Такого устройства нет')
+
+    def _normalize(self, topology_dict):
+        topology_result = topology_dict
+        for key in list(topology_dict.keys()):
+            if key in topology_dict.values():
+                if topology_dict[topology_dict[key]] == key:
+                    topology_result.pop(key)
+        return topology_result
+
+    def __add__(self, other):
+        result = self.topology
+        print(other)
+        for key in list(dict(other)):
+            result[key] = other[key]
+        return Topology(result)
+
+t1 = Topology(topology_example)
+t2 = Topology(topology_example2)
+pprint(t1.topology)
+pprint(t2.topology)
+t3 = t1 + t2
+pprint(t3.topology)
